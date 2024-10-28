8c5a39dd584013fc799c278fd057733a3ca8ed79
Jinse <akop4869@gmail.com>
2024-10-28 19:33:52 +0900

    first commit

---
+++ src/main/java/Refact/CalvinRefact/CalvinRefactApplication.java
@@ -0,0 +1,13 @@
+package Refact.CalvinRefact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalvinRefactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalvinRefactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Refact/CalvinRefact/CalvinRefactApplicationTests.java
@@ -0,0 +1,13 @@
+package Refact.CalvinRefact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalvinRefactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
