e131ded90945b07de6eb8ebdc280f55c2ae2720a
in3kk <akop4869@gmail.com>
2025-04-04 01:17:01 +0900

    uri filter 추가

--- src/main/java/Refact/CalvinRefact/configuration/WebConfig.java
+++ src/main/java/Refact/CalvinRefact/configuration/WebConfig.java
@@ -1,13 +1,42 @@
 package Refact.CalvinRefact.configuration;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String connectPath = "/imgPath/**";
     private String resourcePath = "file:///F:/CalvinUploadFiles/";//로컬
+    public FilterRegistrationBean<OncePerRequestFilter> urlFilter(){//uri 필터
+        FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                String uri = request.getRequestURI();
+                String sanitizedUri = uri.replaceAll("[<>\\[\\]{}|`]","");//특수 문자 제거
+                HttpServletRequestWrapper wrapperRequest = new HttpServletRequestWrapper(request){
+                    @Override
+                    public String getRequestURI(){
+                        return sanitizedUri;
+                    }
+                };
+                filterChain.doFilter(wrapperRequest,response);
+            }
+        });
+        return registrationBean;
+    }
+
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
